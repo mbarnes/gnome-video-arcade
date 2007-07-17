1ed599c35677b5681a763b92a6e3835f2334a4e2
Matthew Barnes <mbarnes@redhat.com>
2007-07-17 18:51:44 +0000

    2007-07-17  Matthew Barnes  <mbarnes@redhat.com>
    	* src/gva-ui.c (action_about_cb):
    	Add a License button to the About dialog.

--- src/gva-ui.c
+++ src/gva-ui.c
@@ -43,6 +43,19 @@ static const gchar *authors[] =
         NULL
 };
 static const gchar *copyright = "Copyright \xC2\xA9 2007 Matthew Barnes";
+static const gchar *license =
+"GNOME Video Arcade is free software; you can redistribute it "
+"and/or modify it under the terms of the GNU General Public License "
+"as published by the Free Software Foundation; either version 3 of "
+"the License, or (at your option) any later version.\n"
+"\n"
+"GNOME Video Arcade is distributed in the hope that it will be "
+"useful, but WITHOUT ANY WARRANTY; without even the implied warranty "
+"of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+"GNU General Public License for more details.\n"
+"\n"
+"You should have received a copy of the GNU General Public License "
+"along with this program.  If not, see <http://www.gnu.org/licenses/>.";
 
 static void
 record_game_exited (GvaProcess *process, gint status, gchar *inpname)
@@ -71,6 +84,8 @@ action_about_cb (GtkAction *action)
                 "version", PACKAGE_VERSION,
                 "comments", _("XMAME Front-End"),
                 "copyright", copyright,
+                "license", license,
+                "wrap-license", TRUE,
                 "authors", authors,
                 "translator-credits", _("translator-credits"),
                 "logo", logo,
